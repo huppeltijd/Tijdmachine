5c26f084d0f5b197fa3b3423023d810da1330e65
huppeltijd <huppeltijd@gmail.com>
2026-06-01 19:28:10 +0200

    Add files via upload

--- photos.js
+++ photos.js
@@ -23,8 +23,8 @@
 // ============================================================
 
 window.PHOTOS = [
-  { url: "images/HPP2024.png", theme: "Terug naar jezelf",                  year: 2024, place: "Harzé" },
-  { url: "images/HPP2023.png", theme: "HPP detectives",    year: 2023, place: "Lokeren" },
+  { url: "", theme: "Maanlanding",                  year: 1969, place: "Maan" },
+  { url: "", theme: "Val van de Berlijnse Muur",    year: 1989, place: "Berlijn" },
   { url: "", theme: "Atoombom op Hiroshima",        year: 1945, place: "Hiroshima" },
   { url: "", theme: "Eerste vlucht Wright Brothers",year: 1903, place: "Kitty Hawk" },
   { url: "", theme: "Aanslag op de Twin Towers",    year: 2001, place: "New York" },
