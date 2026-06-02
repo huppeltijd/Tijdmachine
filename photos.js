cb38ea0d828b1077410630bf5b954fbb786fc071
huppeltijd <huppeltijd@gmail.com>
2026-06-02 22:15:20 +0200

    Update photos.js

--- photos.js
+++ photos.js
@@ -7,11 +7,37 @@
 // ============================================================
 
 window.PHOTOS = [
-  { photos: ["./images/2000/1.jpg", "./images/2000/2.jpg", "./images/2000/3.jpg"], theme: "Schots Weekend", year: 2000, place: "Recht },
+  { photos: ["./images/2000/1.jpg", "./images/2000/2.jpg", "./images/2000/3.jpg"], theme: "Schots Weekend", year: 2000, place: "Recht" },
   { photos: ["./images/1999/1.jpg", "./images/1999/2.jpg", "./images/1999/3.jpg"], theme: "Piraten, Vikings & Mariniers", year: 1999, place: "Middelkerke" },
   { photos: ["./images/1998/1.jpg", "./images/1998/2.jpg", "./images/1998/3.jpg"], theme: "De Remork", year: 1998, place: "Thuin" },
   { photos: ["./images/1997/1.jpg", "./images/1997/2.jpg", "./images/1997/3.jpg"], theme: "Jambers Film", year: 1997, place: "La Reid" },
   { photos: ["./images/1996/1.jpg", "./images/1996/2.jpg", "./images/1996/3.jpg"], theme: "TV-Shows", year: 1996, place: "Purnode" },
+  { photos: ["./images/2026/1.jpg", "./images/2026/2.jpg", "./images/2026/3.jpg"], theme: "HPP 2026", year: 2026, place: "Vul plaats in" },
+  { photos: ["./images/2025/1.jpg", "./images/2025/2.jpg", "./images/2025/3.jpg"], theme: "HPP 2025", year: 2025, place: "Vul plaats in" },
+  { photos: ["./images/2024/1.jpg", "./images/2024/2.jpg", "./images/2024/3.jpg"], theme: "Terug naar jezelf", year: 2024, place: "Harzé" },
+  { photos: ["./images/2023/1.jpg", "./images/2023/2.jpg", "./images/2023/3.jpg"], theme: "HPP Detectives", year: 2023, place: "Lokeren" },
+  { photos: ["./images/2022/1.jpg", "./images/2022/2.jpg", "./images/2022/3.jpg"], theme: "HPP The Oscars", year: 2022, place: "Dilsen Stokkem" },
+  { photos: ["./images/2021/1.jpg", "./images/2021/2.jpg", "./images/2021/3.jpg"], theme: "Hotel Plezante Paljas", year: 2021, place: "Bleialf" },
+  { photos: ["./images/2020/1.jpg", "./images/2020/2.jpg", "./images/2020/3.jpg"], theme: "HPP 2020", year: 2020, place: "Vul plaats in" },
+  { photos: ["./images/2019/1.jpg", "./images/2019/2.jpg", "./images/2019/3.jpg"], theme: "Waardig oud worden", year: 2019, place: "As" },
+  { photos: ["./images/2018/1.jpg", "./images/2018/2.jpg", "./images/2018/3.jpg"], theme: "Hill Billy's", year: 2018, place: "Boutershof" },
+  { photos: ["./images/2017/1.jpg", "./images/2017/2.jpg", "./images/2017/3.jpg"], theme: "Hoofddeksel/Pijl&Boog", year: 2017, place: "Zelem" },
+  { photos: ["./images/2016/1.jpg", "./images/2016/2.jpg", "./images/2016/3.jpg"], theme: "Back to Base Camp", year: 2016, place: "Purnode Tent" },
+  { photos: ["./images/2015/1.jpg", "./images/2015/2.jpg", "./images/2015/3.jpg"], theme: "Zeebonken", year: 2015, place: "Broedertrouw Nederland" },
+  { photos: ["./images/2014/1.jpg", "./images/2014/2.jpg", "./images/2014/3.jpg"], theme: "Nostalgie", year: 2014, place: "Koersel Fonteintje" },
+  { photos: ["./images/2013/1.jpg", "./images/2013/2.jpg", "./images/2013/3.jpg"], theme: "Extreme Outdoor", year: 2013, place: "Grot Vechmaal & Tent Sy" },
+  { photos: ["./images/2012/1.jpg", "./images/2012/2.jpg", "./images/2012/3.jpg"], theme: "HPP Travel", year: 2012, place: "Sint Pieters Voeren" },
+  { photos: ["./images/2011/1.jpg", "./images/2011/2.jpg", "./images/2011/3.jpg"], theme: "HPP 2011", year: 2011, place: "Vul plaats in" },
+  { photos: ["./images/2010/1.jpg", "./images/2010/2.jpg", "./images/2010/3.jpg"], theme: "HPP 2010", year: 2010, place: "Vul plaats in" },
+  { photos: ["./images/2009/1.jpg", "./images/2009/2.jpg", "./images/2009/3.jpg"], theme: "HPP 2009", year: 2009, place: "Vul plaats in" },
+  { photos: ["./images/2008/1.jpg", "./images/2008/2.jpg", "./images/2008/3.jpg"], theme: "HPP 2008", year: 2008, place: "Vul plaats in" },
+  { photos: ["./images/2007/1.jpg", "./images/2007/2.jpg", "./images/2007/3.jpg"], theme: "HPP 2007", year: 2007, place: "Vul plaats in" },
+  { photos: ["./images/2006/1.jpg", "./images/2006/2.jpg", "./images/2006/3.jpg"], theme: "HPP 2006", year: 2006, place: "Vul plaats in" },
+  { photos: ["./images/2005/1.jpg", "./images/2005/2.jpg", "./images/2005/3.jpg"], theme: "HPP 2005", year: 2005, place: "Vul plaats in" },
+  { photos: ["./images/2004/1.jpg", "./images/2004/2.jpg", "./images/2004/3.jpg"], theme: "HPP 2004", year: 2004, place: "Vul plaats in" },
+  { photos: ["./images/2003/1.jpg", "./images/2003/2.jpg", "./images/2003/3.jpg"], theme: "HPP 2003", year: 2003, place: "Vul plaats in" },
+  { photos: ["./images/2002/1.jpg", "./images/2002/2.jpg", "./images/2002/3.jpg"], theme: "HPP 2002", year: 2002, place: "Vul plaats in" },
+  { photos: ["./images/2001/1.jpg", "./images/2001/2.jpg", "./images/2001/3.jpg"], theme: "HPP 2001", year: 2001, place: "Vul plaats in" },
 
   
 ];
