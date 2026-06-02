1ed99dc44296156b1708fe0ab56c99fd9abc97bd
huppeltijd <huppeltijd@gmail.com>
2026-06-02 21:53:14 +0200

    Update photos.js

--- photos.js
+++ photos.js
@@ -7,6 +7,11 @@
 // ============================================================
 
 window.PHOTOS = [
+  { photos: ["./images/2000/1.jpg", "./images/2000/2.jpg", "./images/2000/3.jpg"], theme: "Schots Weekend", year: 2000, place: "Recht },
+  { photos: ["./images/1999/1.jpg", "./images/1999/2.jpg", "./images/1999/3.jpg"], theme: "Piraten, Vikings & Mariniers", year: 1999, place: "Middelkerke" },
+  { photos: ["./images/1998/1.jpg", "./images/1998/2.jpg", "./images/1998/3.jpg"], theme: "De Remork", year: 1998, place: "Thuin" },
+  { photos: ["./images/1997/1.jpg", "./images/1997/2.jpg", "./images/1997/3.jpg"], theme: "Jambers Film", year: 1997, place: "La Reid" },
+  { photos: ["./images/1996/1.jpg", "./images/1996/2.jpg", "./images/1996/3.jpg"], theme: "TV-Shows", year: 1996, place: "Purnode" },
   { photos: ["./images/2026/1.jpg", "./images/2026/2.jpg", "./images/2026/3.jpg"], theme: "HPP 2026", year: 2026, place: "Vul plaats in" },
   { photos: ["./images/2025/1.jpg", "./images/2025/2.jpg", "./images/2025/3.jpg"], theme: "HPP 2025", year: 2025, place: "Vul plaats in" },
   { photos: ["./images/2024/1.jpg", "./images/2024/2.jpg", "./images/2024/3.jpg"], theme: "Terug naar jezelf", year: 2024, place: "Harzé" },
@@ -34,9 +39,5 @@ window.PHOTOS = [
   { photos: ["./images/2003/1.jpg", "./images/2003/2.jpg", "./images/2003/3.jpg"], theme: "HPP 2003", year: 2003, place: "Vul plaats in" },
   { photos: ["./images/2002/1.jpg", "./images/2002/2.jpg", "./images/2002/3.jpg"], theme: "HPP 2002", year: 2002, place: "Vul plaats in" },
   { photos: ["./images/2001/1.jpg", "./images/2001/2.jpg", "./images/2001/3.jpg"], theme: "HPP 2001", year: 2001, place: "Vul plaats in" },
-  { photos: ["./images/2000/1.jpg", "./images/2000/2.jpg", "./images/2000/3.jpg"], theme: "HPP 2000", year: 2000, place: "Vul plaats in" },
-  { photos: ["./images/1999/1.jpg", "./images/1999/2.jpg", "./images/1999/3.jpg"], theme: "HPP 1999", year: 1999, place: "Vul plaats in" },
-  { photos: ["./images/1998/1.jpg", "./images/1998/2.jpg", "./images/1998/3.jpg"], theme: "HPP 1998", year: 1998, place: "Vul plaats in" },
-  { photos: ["./images/1997/1.jpg", "./images/1997/2.jpg", "./images/1997/3.jpg"], theme: "JAMBERS FILM", year: 1997, place: "La Reid" },
-  { photos: ["./images/1996/1.jpg", "./images/1996/2.jpg", "./images/1996/3.jpg"], theme: "TV-SHOWS", year: 1996, place: "Purnode" },
+  
 ];
